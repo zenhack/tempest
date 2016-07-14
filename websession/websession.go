088a9860e7eb613ceee403d1078a5dc5a969b256
Ian Denhardt <ian@zenhack.net>
2016-07-13 21:34:43 -0400

    websession: finish implementing WriteHeader
    I have not really tested this.

--- websession/websession.go
+++ websession/websession.go
@@ -23,32 +23,93 @@ package websession // import "zenhack.net/go/sandstorm/websession"
 // limitations under the License.
 
 import (
+	"bytes"
+	"golang.org/x/net/context"
+	"io"
+	"mime"
 	"net/http"
 	"net/url"
 	"zenhack.net/go/sandstorm/capnp/grain"
-	"zenhack.net/go/sandstorm/capnp/util"
+	capnp_util "zenhack.net/go/sandstorm/capnp/util"
 	capnp "zenhack.net/go/sandstorm/capnp/websession"
+	"zenhack.net/go/sandstorm/util"
 )
 
-func FromHandler(h http.Handler) HandlerWebSession {
-	return HandlerWebSession{h}
+func FromHandler(ctx context.Context, h http.Handler) HandlerWebSession {
+	return HandlerWebSession{ctx, h}
 }
 
 type responseWriter struct {
-	status   int
-	header   http.Header
-	stream   util.ByteStream
-	response *capnp.WebSession_Response
+	status     int
+	header     http.Header
+	ctx        capnp.WebSession_Context
+	body       io.WriteCloser
+	response   *capnp.WebSession_Response
+	webSession HandlerWebSession
 }
 
 func (r *responseWriter) Header() http.Header {
 	return r.header
 }
 
+// value for responesWriter.body where a body is legal, but the capnp schema
+// doesn't provide a way to do streaming. In this case we just buffer the
+// writes and transmit on close.
+//
+// Note that closeCallback needs to be set to a function that will supply the
+// apropriate field.
+type bufBody struct {
+	bytes.Buffer
+	closeCallback func()
+}
+
+func (b *bufBody) Close() error {
+	b.closeCallback()
+	return nil
+}
+
+// io.WriteCloser with both metods NoOps. For responseWriter.body where no body
+// is allowed.
+type noBody struct {
+}
+
+func (b noBody) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func (b noBody) Close() error {
+	return nil
+}
+
+// Set all the headers that are present and accepted by sandstorm. Silently
+// drop any headers sandstorm doesn't support.
+func (r *responseWriter) setSuccessHeaders() {
+	content := r.response.Content()
+	if encoding := r.header.Get("Content-Encoding"); encoding != "" {
+		content.SetEncoding(encoding)
+	}
+	if language := r.header.Get("Content-Language"); language != "" {
+		content.SetLanguage(language)
+	}
+	if mimeType := r.header.Get("Content-Type"); mimeType != "" {
+		content.SetMimeType(mimeType)
+	}
+	if disposition := r.header.Get("Content-Disposition"); disposition != "" {
+		typ, params, err := mime.ParseMediaType(disposition)
+		if err != nil {
+			if typ == "attachment" {
+				content.Disposition().SetDownload(params["filename"])
+			} else {
+				content.Disposition().SetNormal()
+			}
+		}
+	}
+}
+
 func (r *responseWriter) WriteHeader(status int) {
 	r.status = status
-	switch {
-	case status >= 200 && status <= 202:
+	switch status {
+	case 200, 201, 202:
 		r.response.SetContent()
 		// TODO: the subtraction is breaking an abstraction boundary just a bit.
 		// should be safe in this case, but let's think about the implications.
@@ -57,8 +118,58 @@ func (r *responseWriter) WriteHeader(status int) {
 		// TODO: Figure out what we should be passing here; do we actually need to do
 		// anything? Handle_Server is just interface{}, so we're passing in 0, since it's
 		// handy.
-		r.response.Content().Body().SetStream(util.Handle_ServerToClient(0))
-		// TODO: actually set headers, and handle non-200 responses.
+		r.response.Content().Body().SetStream(capnp_util.Handle_ServerToClient(0))
+		r.setSuccessHeaders()
+		r.body = util.ByteStream{r.webSession.Ctx, r.ctx.ResponseStream()}
+	case 204, 205:
+		r.body = noBody{}
+		r.response.SetNoContent()
+		r.response.NoContent().SetShouldResetForm(status == 205)
+	case 301, 302, 303, 307:
+		// Redirects. Web-session.capnp talks about a 308, but I haven't found
+		// any info about its semantics. "net/http" doesn't deifine a constant
+		// for it, so we're just going to say to heck with it and not support
+		// it for now.
+		r.body = noBody{}
+		r.response.SetRedirect()
+		r.response.Redirect().SetLocation(r.header.Get("Location"))
+		switch status {
+		case 301:
+			r.response.Redirect().SetIsPermanent(true)
+		case 302, 303, 307:
+			r.response.Redirect().SetIsPermanent(false)
+		}
+		switch status {
+		case 302, 303:
+			r.response.Redirect().SetSwitchToGet(true)
+		default:
+			r.response.Redirect().SetSwitchToGet(false)
+		}
+	case 400, 403, 404, 405, 406, 409, 410, 413, 414, 415, 418:
+		r.response.SetClientError()
+		capnpStatus := capnp.WebSession_Response_ClientErrorCode(status - 400)
+		r.response.ClientError().SetStatusCode(capnpStatus)
+		buf := &bufBody{}
+		buf.closeCallback = func() {
+			r.response.ClientError().SetDescriptionHtml(buf.String())
+		}
+		r.body = buf
+	default:
+		r.response.SetServerError()
+		if status >= 500 && status < 600 {
+			// The handler actually returned a 5xx; let them set the body.
+			buf := &bufBody{}
+			buf.closeCallback = func() {
+				r.response.ServerError().SetDescriptionHtml(buf.String())
+			}
+			r.body = buf
+		} else {
+			// The client returned some status sandstorm doesn't support. In this
+			// case we just junk the body.
+			//
+			// TODO: Log this somewhere?
+			r.body = noBody{}
+		}
 	}
 }
 
@@ -66,11 +177,11 @@ func (r *responseWriter) Write(p []byte) (int, error) {
 	if r.status == 0 {
 		r.WriteHeader(200)
 	}
-	// TODO: actually implement
-	return 0, nil
+	return r.body.Write(p)
 }
 
 type HandlerWebSession struct {
+	Ctx context.Context
 	http.Handler
 }
 
@@ -94,7 +205,6 @@ func (h HandlerWebSession) Get(args capnp.WebSession_get) error {
 	//checkErr()
 	//accept, err := ctx.Accept()
 	//checkErr()
-	responseStream := ctx.ResponseStream()
 	if firstErr != nil {
 		return firstErr
 	}
@@ -110,12 +220,14 @@ func (h HandlerWebSession) Get(args capnp.WebSession_get) error {
 	}
 
 	respond := responseWriter{
-		header:   make(map[string][]string),
-		stream:   responseStream,
-		response: &args.Results,
+		webSession: h,
+		ctx:        ctx,
+		header:     make(map[string][]string),
+		response:   &args.Results,
 	}
 
 	h.ServeHTTP(&respond, &request)
+	respond.body.Close()
 	return nil
 }
 
