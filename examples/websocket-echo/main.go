869dc21ce28522396174598fcf6db47a1b7e7e66
Ian Denhardt <ian@zenhack.net>
2016-08-30 17:34:12 -0400

    New example: websocket echo server

---
+++ examples/websocket-echo/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"golang.org/x/net/websocket"
+	"io"
+	"log"
+	"net/http"
+	"time"
+	"zenhack.net/go/sandstorm/grain"
+	"zenhack.net/go/sandstorm/websession"
+)
+
+var mainPage = []byte(`<!doctype html>
+<html>
+	<head>
+		<title>Websocket echo server</title>
+		<script>
+		document.addEventListener("DOMContentLoaded", function(event) {
+
+			window.addEventListener("message", function(event) {
+				if (event.data.rpcId !== "0") {
+					return;
+				}
+				if (event.data.error) {
+					console.log("ERROR: " + event.data.error);
+					return;
+				}
+				var elt = document.getElementById("offer-iframe");
+				elt.setAttribute("src", event.data.uri);
+			});
+
+			window.parent.postMessage({
+				renderTemplate: {
+					rpcId: "0",
+					template: window.location.protocol.replace("http", "ws") +
+							"//$API_HOST/.sandstorm-token/$API_TOKEN/connect"
+				}
+			}, "*");
+
+		});
+		</script>
+	</head>
+	<body>
+		<p>Connect to the echo server at:</p>
+
+		<iframe id="offer-iframe"></iframe>
+	</body>
+</html>
+`)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write(mainPage)
+	})
+
+	http.Handle("/connect", websocket.Handler(func(conn *websocket.Conn) {
+		io.Copy(conn, conn)
+	}))
+
+	ctx := context.Background()
+	_, err := grain.ConnectAPI(ctx, websession.FromHandler(ctx, http.DefaultServeMux))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// We don't need to do anything else in this goroutine; our websession
+	// is registered with sandstorm. But, if we fall off the end of main the
+	// program will exit, so let's avoid that:
+	for {
+		time.Sleep(30 * time.Second)
+	}
+}
