3a0e5342e508bb5bb2936f0fddc9d7eec4ddd49d
Ian Denhardt <ian@zenhack.net>
2016-10-25 22:10:14 -0400

    Basic unit test
    We need more of these.

---
+++ websession/websession_test.go
@@ -0,0 +1,45 @@
+package websession
+
+import (
+	"bytes"
+	"golang.org/x/net/context"
+	"io"
+	"net/http"
+	"testing"
+	util_capnp "zenhack.net/go/sandstorm/capnp/util"
+	"zenhack.net/go/sandstorm/capnp/websession"
+	"zenhack.net/go/sandstorm/util"
+)
+
+// Very basic: set up a handler that writes "Hello!\n" to the response body
+// and make sure it comes through on the other side
+func TestGET200(t *testing.T) {
+	ctx := context.Background()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Hello!\n"))
+	})
+	webSession := websession.WebSession_ServerToClient(FromHandler(ctx, mux))
+	buf := &bytes.Buffer{}
+	pipeReader, pipeWriter := io.Pipe()
+	go webSession.Get(ctx, func(p websession.WebSession_get_Params) error {
+		p.SetPath("/")
+		wxCtx, err := p.NewContext()
+		if err != nil {
+			t.Fatal(err)
+			return err
+		}
+		wxCtx.SetResponseStream(
+			util_capnp.ByteStream_ServerToClient(
+				&util.WriteCloserByteStream{WC: pipeWriter}))
+		return nil
+	})
+	_, err := io.Copy(buf, pipeReader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	output := buf.String()
+	if output != "Hello!\n" {
+		t.Fatalf("Expected \"Hello!\\n\" but got %q", output)
+	}
+}
