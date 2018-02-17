573dcadaabff58aac8475408f2d86d76ab9671a7
Ian Denhardt <ian@zenhack.net>
2018-02-17 16:20:55 -0500

    Tweak readme.go for websession.
    The previous comment rendered poorly, and I think simply directing the
    reader to HandlerUiView provides a better starting point anyway.

--- exp/websession/readme.go
+++ exp/websession/readme.go
@@ -1,9 +1,4 @@
 // Package websession facilitates using http.Handlers with web-session.
 //
-// Overview:
-//
-// - HandlerUiView implements new*Session for you; you just need to supply
-//   an http.Handler and an implementation of GetViewInfo.
-// - GetSessionData allows http.Handlers to access session information.
-// - SessionData contains the information passed to the new*Session methods.
+// HandlerUiView is the most central thing exposed by this package.
 package websession
