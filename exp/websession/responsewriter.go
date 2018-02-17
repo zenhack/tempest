e0e9ea13a9178766406189f665ba6d8f648c41cc
Ian Denhardt <ian@zenhack.net>
2018-02-17 16:45:12 -0500

    Factor out logic common to all responses.

--- exp/websession/responsewriter.go
+++ exp/websession/responsewriter.go
@@ -29,11 +29,54 @@ func (w *basicResponseWriter) Header() http.Header {
 	return w.header
 }
 
+func (w *basicResponseWriter) writeHeaderCommon() {
+	// A bit of a hack; afaik there's no public
+	// function for parsing a cookie, but we can
+	// abuse http.Response to do it.
+	cookies := (&http.Response{Header: w.header}).Cookies()
+
+	setCookies, err := w.response.NewSetCookies(int32(len(cookies)))
+	if err != nil {
+		// FIXME figure out the appropriate way to handle this.
+		panic("ERROR: error allocating space for SetCookies.")
+	}
+
+	for i, cookie := range cookies {
+
+		// Note that sandstorm sets all cookies as secure.
+		setCookie := setCookies.At(i)
+		setCookie.SetName(cookie.Name)
+		setCookie.SetValue(cookie.Value)
+		if cookie.Path != "" {
+			setCookie.SetPath(cookie.Path)
+		}
+
+		if cookie.MaxAge != 0 && !cookie.Expires.IsZero() {
+			log.Println("Warning: both cookie.MaxAge and cookie.Expires " +
+				"were set; preferring MaxAge.")
+			// TODO: do we want to be more pedantic about this?
+		}
+
+		if cookie.MaxAge < 0 {
+			setCookie.Expires().SetRelative(0)
+		} else if cookie.MaxAge > 0 {
+			setCookie.Expires().SetRelative(uint64(cookie.MaxAge))
+		} else if !cookie.Expires.IsZero() {
+			setCookie.Expires().SetAbsolute(cookie.Expires.Unix())
+		} else {
+			setCookie.Expires().SetNone()
+		}
+	}
+
+	// TODO: cachePolicy
+}
+
 func (w *basicResponseWriter) WriteHeader(statusCode int) {
 	if w.statusCode != 0 {
 		panic("WriteHeader called twice!")
 	}
 	w.statusCode = statusCode
+	w.writeHeaderCommon()
 	switch statusCode {
 	case 200:
 		w.response.SetContent()
@@ -43,47 +86,8 @@ func (w *basicResponseWriter) WriteHeader(statusCode int) {
 		body := content.Body()
 		body.SetStream(w.cancel)
 
-		// A bit of a hack; afaik there's no public
-		// function for parsing a cookie, but we can
-		// abuse http.Response to do it.
-		cookies := (&http.Response{Header: w.header}).Cookies()
-
-		setCookies, err := w.response.NewSetCookies(int32(len(cookies)))
-		if err != nil {
-			// FIXME figure out the appropriate way to handle this.
-			panic("ERROR: error allocating space for SetCookies.")
-		}
-
-		for i, cookie := range cookies {
-			// Note that sandstorm sets all cookies as secure.
-
-			setCookie := setCookies.At(i)
-			setCookie.SetName(cookie.Name)
-			setCookie.SetValue(cookie.Value)
-			if cookie.Path != "" {
-				setCookie.SetPath(cookie.Path)
-			}
-
-			if cookie.MaxAge != 0 && !cookie.Expires.IsZero() {
-				log.Println("Warning: both cookie.MaxAge and cookie.Expires " +
-					"were set; preferring MaxAge.")
-				// TODO: do we want to be more pedantic about this?
-			}
-
-			if cookie.MaxAge < 0 {
-				setCookie.Expires().SetRelative(0)
-			} else if cookie.MaxAge > 0 {
-				setCookie.Expires().SetRelative(uint64(cookie.MaxAge))
-			} else if !cookie.Expires.IsZero() {
-				setCookie.Expires().SetAbsolute(cookie.Expires.Unix())
-			} else {
-				setCookie.Expires().SetNone()
-			}
-		}
-
 		// TODO:
 		//
-		// * cachePolicy
 		// * encoding
 		// * language
 		// * mimeType
