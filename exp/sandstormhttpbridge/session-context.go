191769b8b3dc3b693842610aba557137274135be
Ian Denhardt <ian@zenhack.net>
2020-02-17 22:23:28 -0500

    Add helper for bridges' getSessionRequest method.

--- exp/sandstormhttpbridge/session-context.go
+++ exp/sandstormhttpbridge/session-context.go
@@ -3,9 +3,22 @@ package sandstormhttpbridge
 import (
 	"net/http"
 
+	"log"
+
 	"zenhack.net/go/sandstorm/capnp/grain"
+	"zenhack.net/go/sandstorm/capnp/powerbox"
 	"zenhack.net/go/sandstorm/capnp/sandstormhttpbridge"
 )
+
+type hasId interface {
+	SetId(string) error
+}
+
+func setId(p hasId, req *http.Request) {
+	val := req.Header.Get("X-Sandstorm-Session-Id")
+	log.Printf("session ID header value: %q", val)
+	p.SetId(val)
+}
 
 func GetSessionContext(
 	bridge sandstormhttpbridge.SandstormHttpBridge,
@@ -14,8 +27,30 @@ func GetSessionContext(
 	res, _ := bridge.GetSessionContext(
 		req.Context(),
 		func(p sandstormhttpbridge.SandstormHttpBridge_getSessionContext_Params) error {
-			p.SetId(req.Header.Get("X-Sandstorm-Session-Id"))
+			setId(p, req)
 			return nil
 		})
 	return res.Context()
+}
+
+func GetSessionRequest(
+	bridge sandstormhttpbridge.SandstormHttpBridge,
+	req *http.Request,
+) powerbox.PowerboxDescriptor_List {
+	resFuture, release := bridge.GetSessionRequest(
+		req.Context(),
+		func(p sandstormhttpbridge.SandstormHttpBridge_getSessionRequest_Params) error {
+			setId(p, req)
+			return nil
+		})
+	defer release()
+	res, err := resFuture.Struct()
+	if err != nil {
+		panic(err)
+	}
+	ret, err := res.RequestInfo()
+	if err != nil {
+		panic(err)
+	}
+	return ret
 }
