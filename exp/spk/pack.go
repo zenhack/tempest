628e958813e7aa07a282848ac22214c6577cf816
Ian Denhardt <ian@zenhack.net>
2020-07-24 17:05:26 -0400

    Basic logic for actually building spks.

---
+++ exp/spk/pack.go
@@ -0,0 +1,37 @@
+package spk
+
+import (
+	"io"
+
+	"zenhack.net/go/sandstorm/capnp/spk"
+
+	"github.com/ulikunitz/xz"
+	"zombiezen.com/go/capnproto2"
+)
+
+// Write an .spk into `dest`, using `archive` as the contents and `key` for
+// signing. The archive must already contain the manifest.
+func packInto(dest io.Writer, key Key, archive spk.Archive) error {
+	sig, err := key.signArchive(archive)
+	if err != nil {
+		return err
+	}
+
+	if _, err = dest.Write(spk.MagicNumber); err != nil {
+		return err
+	}
+
+	compressedDest, err := xz.NewWriter(dest)
+	if err != nil {
+		return err
+	}
+
+	enc := capnp.NewEncoder(compressedDest)
+	if err = enc.Encode(sig.Segment().Message()); err != nil {
+		return err
+	}
+	if err = enc.Encode(archive.Segment().Message()); err != nil {
+		return err
+	}
+	return compressedDest.Close()
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/kr/pretty v0.0.0-20160823170715-cfb55aafdaf3
 	github.com/kr/text v0.0.0-20160504234017-7cafcd837844
+	github.com/ulikunitz/xz v0.5.7
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
 	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
 	zombiezen.com/go/capnproto2 v2.17.1-0.20180404044107-e89f9b7f0213+incompatible
