d169734aeb58b2f7078868d237c5f9d3621114b4
Ian Denhardt <ian@zenhack.net>
2020-08-07 18:35:41 -0400

    Pull in most of the app init logic from docker-spk.
    We may want to tweak/generalize the template; it's had some things
    removed on the assumption that docker-spk apps don't want auto file
    detection, but we may want to provide callers with the ability to use
    that.

---
+++ exp/spk/newApp.go
@@ -0,0 +1,70 @@
+package spk
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"path/filepath"
+)
+
+// Parameters for initializing an application's pkgdef.
+type PkgDefParams struct {
+	AppId AppId
+	Key   Key
+
+	// The capnp schema id for the app's sandstorm-pkgdef.capnp
+	SchemaId uint64
+
+	// The keyring in which to save the gerated key.
+	// Defaults to ~/.sandstorm/sandstorm-keyring
+	KeyringPath string
+
+	// Path to save the package definition. Defaults to
+	// `.sandstorm/sandstorm-pkgdef.capnp`.
+	PkgDefPath string
+}
+
+// Emit a pkgdef with the given parameters, and save the app's key to the keyring.
+func (p *PkgDefParams) Emit() error {
+	keyring := p.KeyringPath
+	if keyring == "" {
+		keyring = os.Getenv("HOME") + "/.sandstorm/sandstorm-keyring"
+	}
+	err := p.Key.AddToFile(keyring)
+	if err != nil {
+		return fmt.Errorf("Error saving key to keyring: %v", err)
+	}
+
+	pkgDefPath := p.PkgDefPath
+	if pkgDefPath == "" {
+		pkgDefPath = ".sandstorm/sandstorm-pkgdef.capnp"
+	}
+	dir := filepath.Dir(pkgDefPath)
+	err = os.MkdirAll(dir, 0644)
+	if err != nil {
+		return err
+	}
+	pkgDefFile, err := os.Create(pkgDefPath)
+	if err != nil {
+		return err
+	}
+	defer pkgDefFile.Close()
+	return pkgdefTemplate.Execute(pkgDefFile, p)
+}
+
+// Generate the information needed to initialize a new app.
+func NewApp() (*PkgDefParams, error) {
+	key, err := GenerateKey(nil)
+	if err != nil {
+		return nil, err
+	}
+	appId, err := key.AppId()
+	if err != nil {
+		return nil, err
+	}
+	return &PkgDefParams{
+		AppId:    appId,
+		Key:      key,
+		SchemaId: rand.Uint64() | (1 << 63),
+	}, nil
+}
