1941df0c683d6df45c562016d3b2f56dc3569e00
Ian Denhardt <ian@zenhack.net>
2022-10-02 21:09:01 -0400

    Code to launch sandstorm-http-bridge apps

--- go/cmd/sandstorm-sandbox-agent/main.go
+++ go/cmd/sandstorm-sandbox-agent/main.go
@@ -8,54 +8,69 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net"
 	"os"
 	"os/exec"
+	"strconv"
 
 	"capnproto.org/go/capnp/v3"
-	"golang.org/x/sys/unix"
 	"zenhack.net/go/sandstorm-next/go/internal/util"
 	"zenhack.net/go/sandstorm/capnp/spk"
 )
 
-func buildCmd(cmd spk.Manifest_Command) (*exec.Cmd, error) {
+type Command struct {
+	Args []string
+	Env  []string
+}
+
+func (c Command) ToOsCmd() *exec.Cmd {
+	ret := exec.Command(c.Args[0], c.Args[1:]...)
+	ret.Env = c.Env
+	return ret
+}
+
+func parseCmd(cmd spk.Manifest_Command) (Command, error) {
 	argv, err := cmd.Argv()
 	if err != nil {
-		return nil, err
+		return Command{}, err
 	}
 	var args []string
 	for i := 0; i < argv.Len(); i++ {
 		arg, err := argv.At(i)
 		if err != nil {
-			return nil, err
+			return Command{}, err
 		}
 		args = append(args, arg)
 	}
 	if len(args) == 0 {
-		return nil, fmt.Errorf("len(cmd.argv) == 0")
+		return Command{}, fmt.Errorf("len(cmd.argv) == 0")
 	}
 	environ, err := cmd.Environ()
 	if err != nil {
-		return nil, err
+		return Command{}, err
 	}
 	var env []string
 	for i := 0; i < environ.Len(); i++ {
 		kv := environ.At(i)
 		k, err := kv.Key()
 		if err != nil {
-			return nil, err
+			return Command{}, err
 		}
 		v, err := kv.Value()
 		if err != nil {
-			return nil, err
+			return Command{}, err
 		}
 		env = append(env, k+"="+v)
 	}
+	return Command{
+		Args: args,
+		Env:  env,
+	}, nil
 
-	ret := exec.Command(args[0], args[1:]...)
-	ret.Env = env
-	return ret, nil
 }
 
 func main() {
@@ -72,15 +87,65 @@ func main() {
 	fmt.Println("App title: ", text)
 	spkCmd, err := manifest.ContinueCommand()
 	util.Chkfatal(err)
-	cmd, err := buildCmd(spkCmd)
+	cmd, err := parseCmd(spkCmd)
 	util.Chkfatal(err)
 
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	fds, err := unix.Socketpair(unix.AF_UNIX, unix.SOCK_STREAM, 0)
+	fmt.Printf("Command: %v", cmd.Args)
+	if cmd.Args[0] != "/sandstorm-http-bridge" {
+		panic("Only sandstorm-http-bridge apps are supported.")
+	}
+	if len(cmd.Args) < 4 {
+		// should be like /sandstorm-http-bridge <port-no> -- /app/command ...args
+		panic("Too few argments")
+	}
+	portNo, err := strconv.Atoi(cmd.Args[1])
 	util.Chkfatal(err)
-	parentSock := os.NewFile(uintptr(fds[0]), "Parent socket")
-	defer parentSock.Close()
-	childSock := os.NewFile(uintptr(fds[1]), "Child socket")
-	cmd.ExtraFiles = []*os.File{childSock}
+	if cmd.Args[2] != "--" {
+		panic("Error: second argument should be '--' separator.")
+	}
+	cmd.Args = cmd.Args[3:]
+	osCmd := cmd.ToOsCmd()
+
+	util.Chkfatal(startCapnpApi())
+
+	util.Chkfatal(osCmd.Start())
+	go func() {
+		defer os.Exit(1)
+		util.Chkfatal(osCmd.Wait())
+		log.Println("App exited; shutting down grain.")
+	}()
+
+	log.Printf("App started on port #%v. TODO: connect to it and do stuff.", portNo)
+
+	<-context.Background().Done()
+
+	/*
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		fds, err := unix.Socketpair(unix.AF_UNIX, unix.SOCK_STREAM, 0)
+		util.Chkfatal(err)
+		parentSock := os.NewFile(uintptr(fds[0]), "Parent socket")
+		defer parentSock.Close()
+		childSock := os.NewFile(uintptr(fds[1]), "Child socket")
+		cmd.ExtraFiles = []*os.File{childSock}
+	*/
+}
+
+func startCapnpApi() error {
+	l, err := net.Listen("unix", "/tmp/sandstorm-api")
+	if err != nil {
+		return err
+	}
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				continue
+			}
+			// TODO: do something with the connection.
+			log.Println("Got a connection to the api socket.")
+			conn.Close()
+		}
+	}()
+	return nil
 }
