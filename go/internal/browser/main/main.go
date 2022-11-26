5f3d5008a94a339f15ae46572fe5b256e4d8f31a
Ian Denhardt <ian@zenhack.net>
2022-11-25 19:08:20 -0500

    Web UI: Make use of vdom.Updater

--- go/internal/browser/main/main.go
+++ go/internal/browser/main/main.go
@@ -32,18 +32,20 @@ func getCapnpApi(ctx context.Context) (*rpc.Conn, external.ExternalApi) {
 
 func view() vdom.VNode {
 	host := js.Global().Get("window").Get("location").Get("host").String()
-	return vb.H("div", vb.A{"class": "main-ui"}, nil,
-		vb.H("div", vb.A{"class": "main-ui__topbar"}, nil,
-			vb.H("p", nil, nil, vb.T("Topbar")),
-		),
-		vb.H("div", vb.A{"class": "main-ui__main"}, nil,
-			vb.H("div", vb.A{"class": "main-ui__sidebar"}, nil,
-				vb.H("p", nil, nil, vb.T("Sidebar")),
+	return vb.H("body", nil, nil,
+		vb.H("div", vb.A{"class": "main-ui"}, nil,
+			vb.H("div", vb.A{"class": "main-ui__topbar"}, nil,
+				vb.H("p", nil, nil, vb.T("Topbar")),
 			),
-			vb.H("iframe", vb.A{
-				"src":   "//grain." + host,
-				"class": "main-ui__grain-iframe",
-			}, nil),
+			vb.H("div", vb.A{"class": "main-ui__main"}, nil,
+				vb.H("div", vb.A{"class": "main-ui__sidebar"}, nil,
+					vb.H("p", nil, nil, vb.T("Sidebar")),
+				),
+				vb.H("iframe", vb.A{
+					"src":   "//grain." + host,
+					"class": "main-ui__grain-iframe",
+				}, nil),
+			),
 		),
 	)
 }
@@ -96,10 +98,14 @@ func Main() {
 		}
 	}()
 
-	body := js.Global().
-		Get("document").
-		Call("getElementsByTagName", "body").
-		Index(0)
-	body.Call("appendChild", view().ToDomNode().Value)
+	body := vdom.DomNode{
+		Value: js.Global().
+			Get("document").
+			Call("getElementsByTagName", "body").
+			Index(0),
+	}
+	up := vdom.NewUpdater(body)
+	defer up.Close()
+	up.Update(view())
 	<-conn.Done()
 }
