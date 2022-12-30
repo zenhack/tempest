b1c377cbe5e99c4e5398a9ed9158fd754ea91dae
Ian Denhardt <ian@zenhack.net>
2022-12-29 22:50:51 -0500

    Test app: set a custom header.

--- go/cmd/test-app/main.go
+++ go/cmd/test-app/main.go
@@ -4,6 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("X-Sandstorm-App-Custom-Header", "some value")
 		w.Write([]byte("Test!"))
 	})
 	panic(http.ListenAndServe(":8000", nil))
