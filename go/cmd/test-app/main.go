cf7987a62010b76666ebc0e8812da3bef236fce1
Ian Denhardt <ian@zenhack.net>
2023-01-08 02:21:11 -0500

    Test app: dump request info to console

--- go/cmd/test-app/main.go
+++ go/cmd/test-app/main.go
@@ -1,10 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		log.Printf("Incoming request. method = %v, headers:", req.Method)
+		for k, vs := range req.Header {
+			for _, v := range vs {
+				log.Printf("%v: %v", k, v)
+			}
+		}
+
 		w.Header().Set("X-Sandstorm-App-Custom-Header", "some value")
+		w.Header().Set("Content-Type", "text/plain")
 		w.Write([]byte("Test!"))
 	})
 	panic(http.ListenAndServe(":8000", nil))
