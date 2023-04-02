580f85c444b6b687726eb550e6e158145b2b2460
Ian Denhardt <ian@zenhack.net>
2023-04-02 17:53:28 -0400

    Delete now unused genLostToken

--- internal/server/database/sturdyref.go
+++ internal/server/database/sturdyref.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"crypto/rand"
-	"crypto/sha256"
 
 	"zenhack.net/go/util"
 )
@@ -12,11 +11,4 @@ func GenToken() []byte {
 	_, err := rand.Read(token[:])
 	util.Chkfatal(err)
 	return token[:]
-}
-
-// Generate a random sturdyRef and return the hash of the token (losing the
-// original token). Useful for stuff that goes in user keyrings, and thus
-// doesn't need to have an actually-usable token.
-func genLostToken() [sha256.Size]byte {
-	return sha256.Sum256(GenToken())
 }
