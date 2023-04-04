449ae2f447583203af32cc7ed50c5ccf3bf53bff
Ian Denhardt <ian@zenhack.net>
2023-04-04 16:49:36 -0400

    Add test for unpack.
    Fails, need to debug.

---
+++ pkg/exp/spk/unpack_test.go
@@ -0,0 +1,86 @@
+package spk
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"zenhack.net/go/util/exn"
+)
+
+const pkgUrlBase = "https://app-index.sandstorm.io/packages/"
+
+type testPackage struct {
+	ID    string
+	Valid bool
+}
+
+var marketPackages = []testPackage{
+	{
+		// Filedrop 1.0.6
+		ID:    "d13f67230d0a4f9a63fc2c0bba87fc52",
+		Valid: true,
+	},
+}
+
+func (p testPackage) FilePath() string {
+	return "testdata/" + p.ID + ".spk"
+}
+
+func (p testPackage) fetchFromAppMarket() ([]byte, error) {
+	return exn.Try(func(throw exn.Thrower) []byte {
+		resp, err := http.Get(pkgUrlBase + p.ID)
+		throw(err)
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			throw(fmt.Errorf(
+				"Failed to fetch package %v; http status = %v",
+				p.ID, resp.StatusCode,
+			))
+		}
+		buf := &bytes.Buffer{}
+		_, err = io.Copy(buf, resp.Body)
+		throw(err)
+		return buf.Bytes()
+	})
+}
+
+func (p testPackage) runTest(t *testing.T, downloadIfNeeded bool) {
+	buf, err := os.ReadFile(p.FilePath())
+	if err != nil {
+		if !downloadIfNeeded {
+			t.Skip("Package ", p.ID, "not present; skipping")
+		}
+		buf, err = p.fetchFromAppMarket()
+		require.NoError(t, err, "fetching package from app market")
+		require.NoError(t, os.WriteFile(p.FilePath(), buf, 0600), "saving package")
+	}
+	tmpDir := t.TempDir()
+	outputDir := tmpDir + "/output-" + p.ID
+	scratchDir := tmpDir + "/scratch-" + p.ID
+	require.NoError(t, os.MkdirAll(outputDir, 755))
+	require.NoError(t, os.MkdirAll(scratchDir, 755))
+	_, _, err = UnpackSpk(outputDir, scratchDir, bytes.NewBuffer(buf))
+	if p.Valid {
+		require.NoError(t, err)
+	} else {
+		require.NotNil(t, err)
+	}
+}
+
+func TestMarketPackages(t *testing.T) {
+	t.Parallel()
+
+	downloadIfNeeded := os.Getenv("NETWORK_TEST_OK") == "1"
+
+	for _, p := range marketPackages {
+		t.Run("package "+p.ID, func(t *testing.T) {
+			t.Parallel()
+			p.runTest(t, downloadIfNeeded)
+		})
+	}
+}
