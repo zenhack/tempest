717dbe93dceec0ad79097a7e953f5dc523bf1973
Ian Denhardt <ian@zenhack.net>
2023-04-23 03:24:18 -0400

    Use TLS config.
    Haven't tested this with an actual cert yet, but it doesn't break
    anything without one.

--- internal/server/main/main.go
+++ internal/server/main/main.go
@@ -1,6 +1,8 @@
 package servermain
 
 import (
+	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -16,20 +18,55 @@ func Main() {
 	initStorage()
 	lg := logging.NewLogger()
 	cfg := ConfigFromSettings(lg)
-	listenAddr := ":" + cfg.HTTP.Port
+	httpAddr := ":" + cfg.HTTP.Port
+	httpsAddr := ":" + cfg.HTTP.TLSPort
 	db := util.Must(database.Open())
 	sessionStore := session.NewStore(util.Must(session.GetKeys()))
 	srv := newServer(cfg, lg, db, sessionStore)
 	defer srv.Release()
 
+	if cfg.HTTP.KeyFile != "" {
+		fi, err := os.Lstat(cfg.HTTP.KeyFile)
+		util.Chkfatal(err)
+		mode := fi.Mode()
+		if mode&0077 != 0 {
+			logging.Panic(lg,
+				"https key file has incorrect permisisons; must have no access by group or other",
+				"permissions", fmt.Sprintf("%o", mode&0777),
+			)
+		}
+	}
+
 	http.Handle("/", srv.Handler())
 	lg.Info("Listening",
 		"root-domain", cfg.HTTP.RootDomain,
-		"listen-addr", listenAddr,
+		"http-addr", httpAddr,
+		"https-addr", httpsAddr,
 	)
-	httpSrv := &http.Server{Addr: listenAddr}
+	httpSrv := &http.Server{Addr: httpAddr}
 	go monitorSignals(httpSrv)
-	panic(httpSrv.ListenAndServe())
+
+	// We can't just use util.Chkfatal for the below, becasue
+	// they *always* return an error -- we have to check which
+	// error:
+	checkServerError := func(err error) {
+		if err != http.ErrServerClosed {
+			panic(err)
+		}
+	}
+
+	if cfg.HTTP.CertFile != "" && cfg.HTTP.KeyFile != "" {
+		l, err := net.Listen("tcp", httpsAddr)
+		util.Chkfatal(err)
+		go func() {
+			checkServerError(httpSrv.ServeTLS(
+				l,
+				cfg.HTTP.CertFile,
+				cfg.HTTP.KeyFile,
+			))
+		}()
+	}
+	checkServerError(httpSrv.ListenAndServe())
 }
 
 func monitorSignals(srv *http.Server) {
