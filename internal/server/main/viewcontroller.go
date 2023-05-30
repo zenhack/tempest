4855d3d9726ec67d7bfe581ecda8fe91bf642879
Ian Denhardt <ian@zenhack.net>
2023-05-30 18:22:16 -0400

    Use exn package in viewcontroller.go

--- internal/server/main/viewcontroller.go
+++ internal/server/main/viewcontroller.go
@@ -8,6 +8,7 @@ import (
 	"zenhack.net/go/tempest/internal/common/types"
 	"zenhack.net/go/tempest/internal/server/database"
 	"zenhack.net/go/tempest/internal/server/session"
+	"zenhack.net/go/util/exn"
 )
 
 type uiViewControllerImpl struct {
@@ -17,43 +18,28 @@ type uiViewControllerImpl struct {
 }
 
 func (c uiViewControllerImpl) MakeSharingToken(ctx context.Context, p external.UiView_Controller_makeSharingToken) error {
-	wantPerms, err := p.Args().Permissions()
-	if err != nil {
-		return err
-	}
-
-	note, err := p.Args().Note()
-	if err != nil {
-		return err
-	}
-
-	results, err := p.AllocResults()
-	if err != nil {
-		return err
-	}
-
-	tx, err := c.DB.Begin()
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-	accountID, err := tx.GetCredentialAccount(c.Session.Credential)
-	if err != nil {
-		return exc.WrapError("no account for credential", err)
-	}
-	perms, err := tx.AccountGrainPermissions(accountID, c.GrainID)
-	if err != nil {
-		return exc.WrapError("failed to fetch permissions", err)
-	}
-	if len(perms) < wantPerms.Len() {
-		perms = perms[:wantPerms.Len()]
-	}
-	for i := range perms {
-		perms[i] = perms[i] && wantPerms.At(i)
-	}
-	token, err := tx.NewSharingToken(accountID, c.GrainID, perms, note)
-	if err != nil {
-		return err
-	}
-	return results.SetToken(token)
+	return exn.Try0(func(throw exn.Thrower) {
+		wantPerms, err := p.Args().Permissions()
+		throw(err)
+		note, err := p.Args().Note()
+		throw(err)
+		results, err := p.AllocResults()
+		throw(err)
+		tx, err := c.DB.Begin()
+		throw(err)
+		defer tx.Rollback()
+		accountID, err := tx.GetCredentialAccount(c.Session.Credential)
+		throw(exc.WrapError("no account for credential", err))
+		perms, err := tx.AccountGrainPermissions(accountID, c.GrainID)
+		throw(exc.WrapError("failed to fetch permissions", err))
+		if len(perms) < wantPerms.Len() {
+			perms = perms[:wantPerms.Len()]
+		}
+		for i := range perms {
+			perms[i] = perms[i] && wantPerms.At(i)
+		}
+		token, err := tx.NewSharingToken(accountID, c.GrainID, perms, note)
+		throw(err)
+		throw(results.SetToken(token))
+	})
 }
